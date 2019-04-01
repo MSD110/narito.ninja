7e1759f24010d8f76fa36f7fa3f097568ea8f50a
naritotakizawa <toritoritorina@gmail.com>
2019-04-01 16:26:23 +0900

    jsをjsディレクトリに

--- blog/widgets.py
+++ blog/widgets.py
@@ -5,11 +5,11 @@ class SuggestTagWidget(forms.SelectMultiple):
     template_name = 'blog/widgets/suggest_tag.html'
 
     class Media:
-        js = ['blog/tag_suggest.js']
+        js = ['blog/js/tag_suggest.js']
 
 
 class SuggestPostWidget(forms.SelectMultiple):
     template_name = 'blog/widgets/suggest_post.html'
 
     class Media:
-        js = ['blog/post_suggest.js']
+        js = ['blog/js/post_suggest.js']
